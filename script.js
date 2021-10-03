8ae59bc1e3d58cfa14e37e8687a667592852947a
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-10-03 23:37:47 +0530

    refactor : defined variables

---
+++ script.js
@@ -0,0 +1,6 @@
+var stockName = document.querySelector("#stock-name");
+var stockPurchasePrice  = document.querySelector("#stock-purchase-price");
+var stockQuantity = document.querySelector("#stock-quantity");
+var btnCheck = document.querySelector("#btn-check");
+var outputElement = document.querySelector("#output");
+
