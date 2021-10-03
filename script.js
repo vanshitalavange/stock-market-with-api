59e0d4b4438093b706ddec80ea76061d940ca4bb
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-10-04 00:16:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ var stockPurchasePrice  = document.querySelector("#stock-purchase-price");
 var stockQuantity = document.querySelector("#stock-quantity");
 var btnCheck = document.querySelector("#btn-check");
 var outputElement = document.querySelector("#output");
+var searchURL = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=";
 
+
+
+
+stockName.addEventListener("input",displayList);
