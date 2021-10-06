886ed93704209fb9701e332a7e11a9e03a674979
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-10-06 16:30:45 +0530

    feat : url function

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 var stockName = document.querySelector("#stock-name");
-var stockPurchasePrice  = document.querySelector("#stock-purchase-price");
+var currentPrice = document.querySelector("#current-price");
+const stockList = document.querySelector("#stock-list");
+var stockPurchasePrice = document.querySelector("#stock-purchase-price");
 var stockQuantity = document.querySelector("#stock-quantity");
 var btnCheck = document.querySelector("#btn-check");
-var outputElement = document.querySelector("#output");
-var searchURL = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=";
+var output = document.querySelector("#output");
+var container = document.querySelector(".input-container");
+var searchURL =
+  "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=";
+var currentPriceURL =
+  "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=";
 
-
-
-
-stockName.addEventListener("input",displayList);
+function getSearchURL() {
+  return searchURL + stockName.value + "&apikey=8NT09YOGTVEE1ZHM";
+}
+function getCurrentPriceURL(symbolOfStock) {
+  return currentPriceURL + symbolOfStock + "&apikey=8NT09YOGTVEE1ZHM";
+}
